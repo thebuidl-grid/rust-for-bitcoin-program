fea74c1c49838298b2ca1c0852333d047072640a
RichoKD <jerrykaduma@gmail.com>
2025-10-30 20:58:12 +0100

    chore: week 2 assignment

--- code/src/main.rs
+++ code/src/main.rs
@@ -4,6 +4,16 @@ mod week_2;
 // entry point to run all topics covered
 fn main() {
     // call functions from session_3 modules
+    println!("Task 1:");
+    week_2::task_1::main();
+
+    println!("\nTask 2:");
+    week_2::task_2::main();
+
+    println!("\nTask 3:");
+    week_2::task_3::main();
+
+    println!("\nOther topics:");
     week_2::function::run();
     week_2::loops::run();
     week_2::primitive_types::run();
