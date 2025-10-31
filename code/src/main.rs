7c951413f2bb8d32ca9090bfe6b5bf178da7fca3
sprtd <sagesdn@gmail.com>
2025-10-31 10:45:44 +0100

    chore: add assignment

--- code/src/main.rs
+++ code/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     // week_2::function::run();
     // week_2::loops::run();
     // week_2::primitive_types::run();
+
+    // assignments
     week_2::task_1::main();
     week_2::task_2::main();
     week_2::task_3::main();
